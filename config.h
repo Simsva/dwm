b865a3b2f3a82a74a3270143c4d316bd0ce97c7b
Simon Ericsson <simsva@protonmail.com>
2021-07-02 23:15:15 +0200

    Patched in swallowing

--- config.h
+++ config.h
@@ -2,18 +2,20 @@
 
 /* constants */
 #define TERMINAL "st"
+#define TERMCLASS "St"
 
 /* appearance */
-static unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static unsigned int gappih       = 10;       /* horiz inner gap between windows */
+static unsigned int gappiv       = 10;       /* vert inner gap between windows */
+static unsigned int gappoh       = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov       = 10;       /* vert outer gap between windows and screen edge */
+static int smartgaps             = 0;        /* 1 means no outer gap when there is only one window */
 
-static unsigned int borderpx  = 1;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int borderpx     = 1;        /* border pixel of windows */
+static unsigned int snap         = 32;       /* snap pixel */
+static const int swallowfloating = 0;        /* 1 means swallow floating windows by default */
+static int showbar               = 1;        /* 0 means no bar */
+static int topbar                = 1;        /* 0 means bottom bar */
 static const char *fonts[] = { 
 	"monospace:size=10",
 	"emoji:pixelsize=10:antialias=true:autohint=true",
@@ -38,9 +40,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 8,       1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           1 },
+	/* class        instance    title            tags mask     isfloating   isterminal     noswallow       monitor */
+	{ "Gimp",       NULL,       NULL,            1 << 8,       1,           0,             0,              -1 },
+	{ "Firefox",    NULL,       NULL,            1 << 2,       0,           0,             0,              -1 },
+	{ TERMCLASS,    NULL,       NULL,            0,            0,           1,             0,              -1 },
+	{ NULL,         NULL,       "Event Tester",  0,            0,           0,             1,              -1 }, /* xev */
 };
 
 /* layout(s) */
